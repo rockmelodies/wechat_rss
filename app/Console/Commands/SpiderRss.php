7e7166279b555902fb8841d91733ce19fdf59e28
Nash-x9 <704005406@qq.com>
2018-12-26 16:48:09 +0800

    更换RSS组件，采集方法更新

--- app/Console/Commands/SpiderRss.php
+++ app/Console/Commands/SpiderRss.php
@@ -6,6 +6,7 @@ use App\Rss;
 use App\RssResource;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use UncleNine\FetchRss;
 
 class SpiderRss extends Command
 {
@@ -43,29 +44,28 @@ class SpiderRss extends Command
         $rss_list = RssResource::where('status',true)->get();
         foreach ($rss_list as $item){
             try{
-                $rss = \Feed::load($item->url);
-                $articles = $rss->item;
+                $rss = (new FetchRss())->parse($item->url);
+
+                //$rss = \Feed::load($item->url);
+                $articles = $rss->items;
             }catch (\Exception $exception){
-                print "Not Allow ,Try Once Atom\r\n";
-                try{
-                    $rss = \Feed::loadAtom($item->url);
-                    $articles = $rss->item;
-                }catch (\Exception $e){
-                    print 'Line:'.$exception->getLine().' Message:'.$exception->getMessage()."\r\n";
-                    continue;
-                }
+                print 'Line:'.$exception->getLine().' Message:'.$exception->getMessage()."\r\n";
+                continue;
             }
+            $length = count($articles);
+            print "Start Fetch $item->url;\r\n";
+            print "Start Insert Db,Count($length)\r\n";
 
             foreach ($articles as $article){
-                print "Start Insert Db\r\n";
-                if($article->title && $article->link){
-                    $title = mb_convert_encoding($article->title,'utf-8');
-                    $url   = mb_convert_encoding($article->link,'utf-8');
+                if($article['title'] && $article['link']){
+
+                    $title = mb_convert_encoding($article['title'],'utf-8');
+                    $url   = mb_convert_encoding($article['link'],'utf-8');
                     Rss::firstOrCreate(['title'=>$title,'url'=>$url],[
-                        'title'     => $title,
-                        'summary'   => mb_convert_encoding($article->description,'utf-8'),
-                        'url'       => $url,
-                        'pub_date'  => date('Y-m-d H:i:s',strtotime(mb_convert_encoding($article->pubDate,'utf-8'))),
+                        'title'     => trim($title),
+                        'summary'   => mb_convert_encoding($article['description'],'utf-8'),
+                        'url'       => trim($url),
+                        'pub_date'  => date('Y-m-d H:i:s',strtotime(mb_convert_encoding($article['pubdate'] ?? date('Y-m-d H:i:s'),'utf-8'))),
                     ]);
                 }
             }
